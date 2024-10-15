d33487efa624496bad585f2a299bf6b1b33eb4df
mikolaj.kowalski <mikser.kowalski@gmail.com>
2024-10-15 11:04:11 +0200

    fix: button placment is now always centered

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ textContent, fontSize, borderRadius }) {
 
     return (
         <>
-            <div className=" h-52 w-1/2 mx-auto mt-8 mb-4">
+            <div className=" h-52 w-2/3 mx-auto mt-8 mb-4">
                 <button style={{
                     fontSize: `${fontSize}px`,
                     borderRadius: `${borderRadius}px`,
